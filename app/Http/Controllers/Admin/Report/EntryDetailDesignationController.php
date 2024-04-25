555a0bd6bcf9ff5069bd6e94aa52fb33dca574cc
Nastya0605 <nastya06051981@gmail.com>
2024-04-25 15:53:49 +0300

    add class for report

---
+++ app/Http/Controllers/Admin/Report/EntryDetailDesignationController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Report;
+
+use App\Http\Controllers\Controller;
+use App\Models\Designation;
+use App\Services\Reports\EntryDetailDesignationService;
+
+class EntryDetailDesignationController extends Controller
+{
+    public function entryDetailDesignation($designation_number,EntryDetailDesignationService $service)
+    {
+        if($designation_number){
+
+            $designation = Designation::where('designation', $designation_number)->first();
+
+            if($designation){
+
+                $service->entryDetailDesignation($designation);
+            }
+        }
+    }
+}

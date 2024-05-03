aad51fe9e6881e592f6f698ed5a887ba5e408bae
Nastya0605 <nastya06051981@gmail.com>
2024-05-03 12:45:34 +0300

    fix report entry detail designation

--- app/Models/Material.php
+++ app/Models/Material.php
@@ -18,6 +18,24 @@ class Material extends Model
     {
         return $this->belongsTo(TypeUnit::class, 'type_unit_id');
     }
-
-
+    public function material()
+    {
+        return $this->hasMany(GroupMaterial::class, 'material_id');
+    }
+    public function materialEntry()
+    {
+        return $this->hasMany(GroupMaterial::class, 'material_entry_id','id');
+    }
+    public function groupMaterials()
+    {
+        return $this->hasMany(GroupMaterial::class, 'material_id');
+    }
+    public function groupMaterialsEntry()
+    {
+        return $this->hasMany(GroupMaterial::class, 'material_entry_id');
+    }
+    public function parentMaterials()
+    {
+        return $this->belongsToMany(Material::class, 'group_materials', 'material_entry_id', 'material_id');
+    }
 }

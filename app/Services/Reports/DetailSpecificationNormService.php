d844574dd800339a214c8e436188d5132d5db6dc
Nastya0605 <nastya06051981@gmail.com>
2024-05-03 12:42:02 +0300

    fix report entry detail designation

--- app/Services/Reports/DetailSpecificationNormService.php
+++ app/Services/Reports/DetailSpecificationNormService.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Services\Reports;
+use App\Models\DesignationMaterial;
+use App\Models\GroupMaterial;
+use App\Models\Material;
 use App\Models\ReportApplicationStatement;
 use App\Services\HelpService\PDFService;
 use App\Services\Statements\ApplicationStatementPrintService;
@@ -18,47 +21,89 @@ class DetailSpecificationNormService
 
     public function getData($department,$order_number)
     {
-        $items = ReportApplicationStatement
-            ::where('order_number',$order_number)
-            /*->whereHas('designation', function ($query) use ($department){
-            $query-> whereRaw("SUBSTRING(route, 1, 2) = '$department'");
-        })*/
-            ->has('designationMaterial.material')
-            ->with('designationEntry','designationMaterial.material')
+        /* $items = ReportApplicationStatement
+             ::where('order_number',$order_number)
+             /*->whereHas('designation', function ($query) use ($department){
+             $query-> whereRaw("SUBSTRING(route, 1, 2) = '$department'");
+         })*/
+        /*   ->has('designationMaterial.material')
+           ->with('designationEntry','designationMaterial.material')
+           ->orderBy('order_designationEntry_letters')
+           ->orderBy('order_designationEntry')
+           ->get();*/
+
+        $items = Material::query()
+            // ->leftJOIN('group_materials', 'materials.id', '=','group_materials.material_id')
+            ->join('designation_materials', 'materials.id', '=', 'designation_materials.material_id')
+            ->join('report_application_statements', 'report_application_statements.designation_entry_id', '=', 'designation_materials.designation_id')
+            ->join('designations', 'designations.id', '=', 'report_application_statements.designation_entry_id')
+            ->where('report_application_statements.order_number', $order_number)
+            ->select('materials.*', 'designations.designation as designation_name', 'report_application_statements.quantity_total', 'designation_materials.norm', 'report_application_statements.designation_entry_id')
+            ->orderBy('materials.name')
             ->orderBy('order_designationEntry_letters')
             ->orderBy('order_designationEntry')
+            ->with('groupMaterials')
             ->get();
-
-        $data = $items->sortBy('designationMaterial.material.id')->map(function ($item) {
+;
+        $data = $items->sortBy('id')->map(function ($item) {
             return [
-                'id' => $item->designationMaterial->material->id,
-                'material_name' => $item->designationMaterial->material->name,
-                'detail_name' => $item->designationEntry->designation,
+                'id' => $item->id,
+                'material_name' => $item->name,
+                'detail_name' => $item->designation_name,
                 'quantity_total' => $item->quantity_total,
-                'unit' => $item->designationMaterial->material->unit->unit,
-                'norm' => $item->designationMaterial->norm,
+                'unit' => $item->unit->unit,
+                'norm' => $item->norm,
+                'group_materials' => $item->groupMaterials
             ];
         });
 
         $groupedData = $data->groupBy('id')->map(function ($group) {
-            // Внутри каждой группы по материалу, группируем по detail_name
+            // Группируем внутри каждой группы по названию материала
+            //return $group->groupBy('material_name')->map(function ($materialDetails, $materialName) {
+                // Группируем далее по наименованию детали
             return $group->groupBy('detail_name')->map(function ($details) {
+                // Возвращаем детали для каждой детали в группе
                 return [
-                    'id' => $details->first()['id'], // ID материала из первого элемента
-                    'material_name' => $details->first()['material_name'], // Имя материала из первого элемента
-                    'detail_name' => $details->first()['detail_name'], // Наименование детали
-                    'quantity_total' => $details->sum('quantity_total'), // Сумма quantity_total
-                    'unit' => $details->first()['unit'], // Единица измерения из первого элемента
-                    'norm' => $details->first()['norm'], // Сумма норм для группы
+                    'id' => $details->first()['id'],
+                    'material_name' => $details->first()['material_name'],
+                    'detail_name' => $details->first()['detail_name'],
+                    'quantity_total' => $details->sum('quantity_total'),
+                    'unit' => $details->first()['unit'],
+                    'norm' => $details->first()['norm'],
+                    'group_materials' => $details->first()['group_materials']
                 ];
             });
         });
-     
-        return $groupedData;
+
+
+        $new_array = array();
+        foreach($groupedData as $material_id=>$groupedData_material){
+
+            $group_materials = GroupMaterial::where('material_id',$material_id)->get();
+            if($group_materials->count() > 0) {
+                $group_materials->load('materialEntry');
+
+                foreach ($group_materials as $group) {
+
+                    foreach($groupedData_material as $detail){
+                        $detail['norm'] = $detail['norm']*$group->norm;
+                        $new_array[$group->material_entry_id."_Group"][$group->materialEntry->name][] = $detail;
+                    }
+                }
+            }else{
+                foreach($groupedData_material as $detail){
+                    $new_array[$material_id][$detail['material_name']][] = $detail;
+
+                }
+            }
+        }
+
+        return $new_array;
     }
 
     public function getPdf($data,$department,$order_number)
     {
+
         $width = array(80,40,30,20,50,50);
         $header1 = ['Найменування матеріалу',
             'Найменування DSE',
@@ -71,7 +116,7 @@ class DetailSpecificationNormService
             '',
             '',
             '',
-            'на един',
+            'на один',
             ''];
 
         $pdf = PDFService::getPdf($header1,$header2,$width,'ПОДЕТАЛЬНО-СПЕЦИФІКОВАННІ НОРМИ ВИТРАТ МАТЕРІАЛІВ НА ВИРІБ','ЦЕХ '.$department.' ЗАКАЗ '.$order_number);
@@ -84,42 +129,54 @@ class DetailSpecificationNormService
         $pdf->SetFont('dejavusans', '', 10);
         $sum_norm = 0;
 
-        $first = 1;
-        // Переменная для хранения предыдущего материала
-        $previousMaterial = null;
-        //dd($data);
-        foreach ($data as $row) {
-                
+        foreach ($data as $material_id=>$row) {
+
             if($pdf->getY() >= 185) {
                 $pdf->Cell(0, 5, 'ЛИСТ '.$page,0,1,'C'); // 'C' - выравнивание по центру, '0' - без рамки, '1' - переход на новую строку
                 $pdf = PDFService::getHeaderPdf($pdf, $header1, $header2, $width);
                 $page++;
             }
+            foreach($row as $material_name=>$detail_){
 
-            $key = 0;
-            foreach($row as $detail){
-                if($key == 0){
-                    $pdf->Cell(100, 10, $detail['material_name']);
-                    $pdf->Ln();
-                }
-                $key++;
-                $pdf->Cell($width[0], 10, '');
-                $pdf->Cell($width[1], 10, $detail['detail_name']);
-                $pdf->Cell($width[2], 10, $detail['quantity_total']);
-                $pdf->Cell($width[3], 10, $detail['unit']);
-                $pdf->Cell($width[4], 10, $detail['norm']);
-                $pdf->Cell($width[5], 10, $detail['norm']*$detail['quantity_total']);
+                $pdf->Cell(100, 10, $material_name);
                 $pdf->Ln();
-                $sum_norm = $sum_norm + $detail['norm']*$detail['quantity_total'];
-           }
+                foreach($detail_ as $detail) {
+
+                    $pdf->Cell($width[0], 10, '');
+                    $pdf->Cell($width[1], 10, $detail['detail_name']);
+                    $pdf->Cell($width[2], 10, $detail['quantity_total']);
+                    $pdf->Cell($width[3], 10, $detail['unit']);
+                    $pdf->Cell($width[4], 10, $detail['norm']);
+                    $pdf->Cell($width[5], 10, $detail['norm'] * $detail['quantity_total']);
+                    $pdf->Ln();
+                    $sum_norm = $sum_norm + $detail['norm'] * $detail['quantity_total'];
+                }
+            }
+
             $pdf->Cell(270, 10, 'Разом по матер. '.$sum_norm,0,1,'R');
             $pdf->Ln();
             $sum_norm = 0;
-            
+
         }
-     
+
         // Выводим PDF в браузер
         $pdf->Output('example.pdf', 'I');
     }
 
+    public function detail($detail_,$pdf,$width)
+    {
+        $sum_norm = 0;
+        foreach($detail_ as $detail) {
+
+            $pdf->Cell($width[0], 10, '');
+            $pdf->Cell($width[1], 10, $detail['detail_name']);
+            $pdf->Cell($width[2], 10, $detail['quantity_total']);
+            $pdf->Cell($width[3], 10, $detail['unit']);
+            $pdf->Cell($width[4], 10, $detail['norm']);
+            $pdf->Cell($width[5], 10, $detail['norm'] * $detail['quantity_total']);
+            $pdf->Ln();
+            $sum_norm = $sum_norm + $detail['norm'] * $detail['quantity_total'];
+        }
+        return $sum_norm;
+    }
 }

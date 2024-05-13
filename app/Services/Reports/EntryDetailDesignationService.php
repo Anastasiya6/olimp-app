32f5e5a95a51a114eb12b6f5198d80cd0b47903c
Nastya0605 <nastya06051981@gmail.com>
2024-05-13 14:43:38 +0300

    fix report entry detail designation

--- app/Services/Reports/EntryDetailDesignationService.php
+++ app/Services/Reports/EntryDetailDesignationService.php
@@ -1,14 +1,11 @@
 <?php
 
 namespace App\Services\Reports;
-use App\Models\Designation;
-use App\Models\ReportApplicationStatement;
 use App\Models\Specification;
 use App\Services\HelpService\PDFService;
 
 class EntryDetailDesignationService
 {
-
     public $height = 10;
 
     public $max_height = 10;
@@ -46,7 +43,25 @@ class EntryDetailDesignationService
         // Добавление названия детали
         $this->pdf->MultiCell($this->width[0], $this->height, $designation->designation, 0, 'L', 0, 0, '', '', true, 0, false, true, $this->max_height, 'T');
         $this->pdf->MultiCell($this->width[1], $this->height, $designation->name, 0, 'L', 0, 0, '', '', true, 0, false, true, $this->max_height, 'T');
-        $this->pdf->Ln();
+        $not_first_material = 0;
+        if($designation->designationMaterial->isNotEmpty()) {
+            foreach ($designation->designationMaterial as $material) {
+                if($not_first_material == 1){
+                    $this->pdf->MultiCell($this->width[0], $this->height,'', 0, 'L', 0, 0, '', '', true, 0, false, true, $this->max_height, 'T');
+                    $this->pdf->MultiCell($this->width[1], $this->height, '', 0, 'L', 0, 0, '', '', true, 0, false, true, $this->max_height, 'T');
+                }
+                $this->pdf->MultiCell($this->width[2], $this->height, '', 0, 'L', 0, 0, '', '', true, 0, false, true, $this->max_height, 'T');
+                $this->pdf->MultiCell($this->width[3], $this->height, '', 0, 'L', 0, 0, '', '', true, 0, false, true, $this->max_height, 'T');
+                $this->pdf->MultiCell($this->width[4], $this->height, $material->material->name, 0, 'L', 0, 0, '', '', true, 0, false, true, $this->max_height, 'T');
+                $this->pdf->MultiCell($this->width[5], $this->height, $material->norm . " х", 0, 'L', 0, 0, '', '', true, 0, false, true, $this->max_height, 'T');
+                $this->pdf->MultiCell($this->width[6], $this->height, "1", 0, 'L', 0, 0, '', '', true, 0, false, true, $this->max_height, 'T');
+                $this->pdf->MultiCell($this->width[7], $this->height, "=".$material->norm, 0, 'L', 0, 0, '', '', true, 0, false, true, $this->max_height, 'T');
+                $this->pdf->Ln();
+                $not_first_material++;
+            }
+        }else{
+            $this->pdf->Ln();
+        }
         $this->node($designation->id,0,1);
 
         $pdf_path = storage_path('app/public/entry_detail_'.$designation->designation.'.pdf');
@@ -62,7 +77,7 @@ class EntryDetailDesignationService
             ::where('designation_id', $designation_id)
             ->with(['designationEntry','designationMaterial'])
             ->get();
-           // dd($specifications);
+
         //$specifications = $specifications->sortBy('category_code');
         $specifications = $specifications->sortBy('detail');
         // Проверяем, что коллекция не пуста
@@ -73,10 +88,10 @@ class EntryDetailDesignationService
                 $this->pdf->MultiCell($this->width[1], $this->height, $designation, 0, 'L', 0, 0, '', '', true, 0, false, true, $this->max_height, 'T');
                 $this->pdf->MultiCell($this->width[2], $this->height, $designation_name, 0, 'L', 0, 0, '', '', true, 0, false, true, $this->max_height, 'T');
                 $this->pdf->Ln();
+
             }
 
             foreach ($specifications as $specification) {
-
                 $this->pdf->MultiCell($this->width[0], $this->height, $designation, 0, 'L', 0, 0, '', '', true, 0, false, true, $this->max_height, 'T');
                 $this->pdf->MultiCell($this->width[1], $this->height, '', 0, 'L', 0, 0, '', '', true, 0, false, true, $this->max_height, 'T');
                 $this->pdf->MultiCell($this->width[2], $this->height, $specification->designationEntry->designation, 0, 'L', 0, 0, '', '', true, 0, false, true, $this->max_height, 'T');

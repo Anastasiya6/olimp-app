12f27ef24815cc626e236a6f9d1784dcc72012f1
Nastya0605 <nastya06051981@gmail.com>
2024-05-16 12:58:14 +0300

    fix navigation menu on public

--- resources/views/layouts/navigation-public.blade.php
+++ resources/views/layouts/navigation-public.blade.php
@@ -2,7 +2,7 @@
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div class="relative flex h-16 items-center justify-between">
             <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div class="hidden sm:ml-6 sm:block">
+                <div class="sm:ml-6 sm:block">
                     <div class="flex space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
 
@@ -15,7 +15,7 @@
 
             <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <div class="flex">
-                    <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <div class="sm:flex sm:items-center sm:ms-6">
 
                         <a href="{{route('admin.home')}}" wire:navigate class="user__menu-admin" title="Administrator panel" target="_blank">
                             <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">

20893e16b48976fede360dd865900b1400306587
Nastya0605 <nastya06051981@gmail.com>
2024-08-01 10:21:21 +0300

    add write off

---
+++ app/Http/Controllers/Admin/Report/ReportWriteOffController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Report;
+
+use App\Http\Controllers\Controller;
+use App\Services\Reports\WriteOffService;
+
+class ReportWriteOffController extends Controller
+{
+    public function writeOff($ids,$order_number,$start_date,$end_date,$sender_department,$receiver_department,WriteOffService $service)
+    {
+        $service->writeOff($ids,$order_number,$start_date,$end_date,$sender_department,$receiver_department);
+    }
+}

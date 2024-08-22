bd8f592f4e31a31bbd12d67919f8aaa91ecba25f
Nastya0605 <nastya06051981@gmail.com>
2024-08-22 14:48:36 +0300

    fix menu in admin

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -26,10 +26,54 @@
             </div>
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <x-nav-link align="right" width="48" :href="route('order-names.index')">
+                    {{ __('Замовлення') }}
+                </x-nav-link>
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>Olimp</div>
+                            <div>Вироби</div>
+
+                            <div class="ms-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <x-dropdown-link wire:navigate :href="route('specifications.index')">
+                            {{ __('M0020') }}
+                        </x-dropdown-link>
+                        <x-dropdown-link wire:navigate :href="route('group-materials.index')">
+                            {{ __('Матеріалокомплекти') }}
+                        </x-dropdown-link>
+                        <x-dropdown-link wire:navigate :href="route('designations.index')">
+                            {{ __('Вироби') }}
+                        </x-dropdown-link>
+                        <x-dropdown-link wire:navigate :href="route('pi0s.index')">
+                            {{ __('ПИ0') }}
+                        </x-dropdown-link>
+                        <x-dropdown-link wire:navigate :href="route('orders.index')">
+                            {{ __('Розузловування') }}
+                        </x-dropdown-link>
+                        <!-- Authentication -->
+                        {{--<form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>--}}
+                    </x-slot>
+                </x-dropdown>
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                            <div>Матеріали</div>
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -43,21 +87,36 @@
                         <x-dropdown-link wire:navigate :href="route('designation-materials.index')">
                             {{ __('Норми') }}
                         </x-dropdown-link>
-                        <x-dropdown-link wire:navigate :href="route('specifications.index')">
-                            {{ __('M0020') }}
-                        </x-dropdown-link>
                         <x-dropdown-link wire:navigate :href="route('materials.index')">
                             {{ __('Матеріали') }}
                         </x-dropdown-link>
-                        <x-dropdown-link wire:navigate :href="route('group-materials.index')">
-                            {{ __('Матеріалокомплекти') }}
-                        </x-dropdown-link>
-                        <x-dropdown-link wire:navigate :href="route('designations.index')">
-                            {{ __('Вироби') }}
-                        </x-dropdown-link>
-                        <x-dropdown-link wire:navigate :href="route('pi0s.index')">
-                            {{ __('ПИ0') }}
-                        </x-dropdown-link>
+
+                        <!-- Authentication -->
+                        {{--<form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>--}}
+                    </x-slot>
+                </x-dropdown>
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                            <div>План</div>
+
+                            <div class="ms-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
+
+                    <x-slot name="content">
                         <x-dropdown-link wire:navigate :href="route('plan-tasks.index')">
                             {{ __('План') }}
                         </x-dropdown-link>
@@ -67,17 +126,37 @@
                         <x-dropdown-link wire:navigate :href="route('write-offs.index')">
                             {{ __('Списання') }}
                         </x-dropdown-link>
-                        <x-dropdown-link wire:navigate :href="route('orders.index')">
-                            {{ __('Розузловування') }}
-                        </x-dropdown-link>
-                        <x-dropdown-link wire:navigate :href="route('reports.index')">
-                            {{ __('Звіти') }}
-                        </x-dropdown-link>
-                        <x-dropdown-link wire:navigate :href="route('order-names.index')">
-                            {{ __('Замовлення') }}
-                        </x-dropdown-link>
                         <x-dropdown-link wire:navigate :href="route('purchases.index')">
                             {{ __('Покупні') }}
+                        </x-dropdown-link>
+                        <!-- Authentication -->
+                        {{--<form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>--}}
+                    </x-slot>
+                </x-dropdown>
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                            <div>Звіти</div>
+
+                            <div class="ms-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <x-dropdown-link wire:navigate :href="route('reports.index')">
+                            {{ __('Звіти') }}
                         </x-dropdown-link>
                         <x-dropdown-link wire:navigate :href="route('specification-logs.index')">
                             {{ __('Зміни у специфікації') }}
